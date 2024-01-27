d70a0df0bde1eb35af5c649e18f65e2aa86dcdad
Enza <e.eziz5@yandex.ru>
2024-01-27 12:13:41 +0500

    Yeah

--- app/[locale]/popular/page.jsx
+++ app/[locale]/popular/page.jsx
@@ -1,13 +1,19 @@
 import IndexSwiper from 'components/Functions/IndexSwiper'
 import ProductContainer from 'components/Containers/ProductContainer/ProductContainer'
 
-export default function NewStuffPage(){
+export default function PopularPage(){
+    const i = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,20,21,22,23,24,25,36]
+
     return(
-        <div className='flex flex-col gap-4 mt-4 max-width'>
-            <h2 className='bg-grey-50 rounded-[4px] center text-xl text-fancy-700 font-bold px-2 h-10 w-fit'>Iň köp halananlar</h2>
+        <div className='flex flex-col gap-4 py-4 max-width'>
+            <h2 className='h2-headers'>Iň köp halananlar</h2>
             <IndexSwiper/>
-            <div className='grid-container'>
-                
+            <div className='grid-template-rows'>
+                {i.map(item => {
+                    return(
+                        <ProductContainer key={item.id}/>
+                    )
+                })}
             </div>
         </div>
     )
---
+++ components/Nav/Highlights.jsx
@@ -0,0 +1,18 @@
+export default function Highlights(){
+    return(
+        <div className='bg-green-400 flex flex-row flex-wrap items-center max-width'>
+            <div className='bg-red-400 rounded-lg center text-white h-20 sm:w-1/2 md:w-1/4 w-full'>
+                <h2 className='text-xl italic font-bold'>7/24 Online</h2>
+            </div>
+            <div className='bg-red-400 rounded-lg center text-white h-20 sm:w-1/2 md:w-1/4 w-full'>
+                <h2 className='text-xl italic font-bold'>7/24 Online</h2>
+            </div>
+            <div className='bg-red-400 rounded-lg center text-white h-20 sm:w-1/2 md:w-1/4 w-full'>
+                <h2 className='text-xl italic font-bold'>7/24 Online</h2>
+            </div>
+            <div className='bg-red-400 rounded-lg center text-white h-20 sm:w-1/2 md:w-1/4 w-full'>
+                <h2 className='text-xl italic font-bold'>7/24 Online</h2>
+            </div>
+        </div>
+    )
+}
